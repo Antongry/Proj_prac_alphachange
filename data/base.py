e310ec1d6ec280c7f3eea5b4b18c041760ecce29
Antongry <gruzdew.anton.ru@gmail.com>
2021-10-12 00:00:46 +0300

    refactor and isaac

--- data/base.py
+++ data/base.py
@@ -3,9 +3,9 @@ with open("jane_eyre.txt", "r") as f:
 
 raw_text = raw_text.lower()
 raw_text = raw_text.replace('\n\n', '\n')
-for i in range(40):
+for i in range(40, 100):
     text = raw_text[:256]
     raw_text = raw_text[257:]
     text = text[text.find(' ') + 1:text.rfind(' ')]
-    with open("./raw_text/{0}.txt".format(i), "w") as out_:
+    with open("./raw_text/novel/{0}.txt".format(i), "w") as out_:
         out_.write(text)
