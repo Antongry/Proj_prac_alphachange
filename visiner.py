79531c129239d6e6ff0548d012b78feacfaa5520
Antongry <gruzdew.anton.ru@gmail.com>
2021-10-11 22:38:40 +0300

    i hope i didn't broke anything

--- visiner.py
+++ visiner.py
@@ -1,56 +1,53 @@
 def find_pos(symbol, alphabet):
     for i in range(len(alphabet)):
-        if alphabet[i]==symbol:
+        if alphabet[i] == symbol:
             return i
     return -1
-  
-  
-  
-  def visiner(s, key, side, alphabet):
+
+
+def visiner(s, key, side, alphabet):
     ans = ''
-    
-    #make table
-    
+
+    # make table
+
     table = [alphabet]
-    for i in range(len(alphabet)-1):
-        alphabet = alphabet[1::]+alphabet[0]
+    for i in range(len(alphabet) - 1):
+        alphabet = alphabet[1::] + alphabet[0]
         table.append(alphabet)
-    alphabet = alphabet[1::]+alphabet[0]
-    
-    
-    #convert key
-    k=[]
+    alphabet = alphabet[1::] + alphabet[0]
+
+    # convert key
+    k = []
     for i in range(len(key)):
         a = find_pos(key[i], alphabet)
         if a != -1:
             k.append(a)
-            
-    #crypt
+
+    # crypt
     last_line = 0
-    line=last_line
-    j = 0      #current sdvig
-    
+    line = last_line
+    j = 0  # current sdvig
+
     for i in range(len(s)):
         a = find_pos(s[i], table[last_line])
         if a != -1:
-            #find line
+            # find line
             line = last_line + side * k[j]
-            
+
             if line >= len(alphabet):
                 line -= len(alphabet)
             if line < 0:
                 line += len(alphabet)
-                
+
             if j >= len(k):
                 j -= len(k)
             if j < 0:
-                j += len(k)    
-                
-            #samo koldynstvo
-            
-            ans+=table[line][a]
+                j += len(k)
+
+                # samo koldynstvo
+
+            ans += table[line][a]
             last_line = line
         else:
-          ans+=s[i]
+            ans += s[i]
     return ans
-    
