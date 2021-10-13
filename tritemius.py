56efc4290e20e25e8c1ed67a9c4dc6a1f8f67d41
Antongry <gruzdew.anton.ru@gmail.com>
2021-10-13 19:27:29 +0300

    bug fixed

--- tritemius.py
+++ tritemius.py
@@ -12,7 +12,7 @@ for i in range(100):
     counter = 0
     for j in range(len(text)):
         if text[j] in a:
-            ans += data[counter % len(a)][a.find(text[i])]
+            ans += data[counter % len(a)][a.find(text[j])]
             counter += 1
         else:
             ans += text[j]
