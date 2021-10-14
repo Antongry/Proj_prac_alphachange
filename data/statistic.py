8e0dd427964603c33ca56fc9740d581b03dfb25f
0-WhiteRabbit <91686700+0-WhiteRabbit@users.noreply.github.com>
2021-10-14 06:17:29 +0000

    Update statistic.py

--- data/statistic.py
+++ data/statistic.py
@@ -1,5 +1,10 @@
+# 0 - 39: jane_eyre
+# 40 - 86: onegin
+# 87 - 99 random
 a = ''
 b = 'ёйцукенгшщзхъфывапролджэячсмитьбю'
+ch_raw = [0] * 33
+ch_enc = [0] * 33
 
 for i in sorted(b):
     a += i
@@ -10,9 +15,18 @@ for i in range(100):
     with open("./raw_text/{0}.txt".format(i), "r") as f1:
         text1 = f1.read()
 
-    data = 'буква     enc | raw\n'
+    data = 'буква       raw    |   enc\n'
+    
     for j in range(len(a)):
+        chance_raw = text1.count(a[j])/len(text1)
+        chance_enc = text.count(a[j])/len(text)
+        ch_raw[j] += chance_raw
+        ch_enc[j] += chance_enc
+
         data += '  {0}:       '.format(a[j])
-        data += '{0}  |  {1}\n'.format(text.count(a[j]), text1.count(a[j]))
-    with open("./statistics/{0}.txt", "w") as f2:
+        data += '{0}  |  {1}\n'.format("%.4f" % (chance_raw), "%.4f" % (chance_enc))
+
+    with open("./statistics/{0}.txt".format(i), "w") as f2:
         f2.write(data)
+
+
